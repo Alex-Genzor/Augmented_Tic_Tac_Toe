58bd2be23ae28188ccce1241da69efd78d23d354
Alexandros Genzor <agenzors@campusdigitalfp.com>
2025-05-22 21:12:28 +0200

    small thing to try to 'talk' between unity inspector and 2d array

--- Assets/_MAIN/Scripts/TicTacToeManager.cs
+++ Assets/_MAIN/Scripts/TicTacToeManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class TicTacToeManager : MonoBehaviour
 {
     public bool turn;
-    public Component[] Components;
+    public Component[][] Components;
+
+    [SerializeField] private Component[] _componentSub1, _componentSub2, _componentSub3;
+    
     // Start is called before the first frame update
     void Start()
     {
+        // Components[0] = _componentSub1;
+        // Components[1] = _componentSub2;
+        // Components[2] = _componentSub3;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         
     }
     
